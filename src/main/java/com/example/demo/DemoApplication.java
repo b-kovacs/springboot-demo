f8264fa8767ba536fa46da9b004bf8f164f96c96
bkovacs <kovacs.office846@gmail.com>
2026-09-06 15:35:07 +0200

    Add /version endpoint
    End-to-end test of the new CI/CD trigger + Flux image automation loop.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,24 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+@RestController
+class VersionController {
+
+	@GetMapping("/version")
+	public String version() {
+		return "CI/CD + Flux image automation end-to-end test";
 	}
 
 }
